4dde8a2048d23b30a56109762a1b67d56216d72d
Денис Шелыгин <113241502+denshelygin@users.noreply.github.com>
2023-10-03 15:08:02 +0300

    Add files via upload

---
+++ DZ_6/Catalog.java
@@ -0,0 +1,10 @@
+package DZ6.DZ_6;
+import java.util.*;
+
+public class Catalog extends Laptop {
+
+    public Catalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+ 
+}
